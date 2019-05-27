d3726c0126f6bf022f4d3cf2efba18712ee760c1
Ernesto Miguel Rodriguez Rodriguez <ernestomr87@gmail.com>
2019-05-27 09:11:14 -0500

    Update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    // api: 'http://geoweb.enpa.vcl.minag.cu',
-    api: 'http://192.168.1.112:3001',
+    api: 'http://geoweb.enpa.vcl.minag.cu',
+    // api: 'http://192.168.1.112:3001',
     geoServerWms: 'http://geoservicios.enpa.vcl.minag.cu/geoserver/ws_ide_vcl/wms'
 
 }
--- src/data/bases.js
+++ src/data/bases.js
@@ -4,10 +4,5 @@ export default [{
 	nomenclature: null,
 	wms:'ws_ide_vcl:limite_municipal',
 	json: 'http://geoservicios.enpa.vcl.minag.cu/geoserver/ws_ide_vcl/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ws_ide_vcl:limite_municipal&outputFormat=application%2Fjson'
-},
-{
-	name: 'Límites Municipaless',
-	nomenclature: null,
-	json: 'http://localhost:3001/api/sitema_agricola/forma_prodictiva/725637272/usoTenencia'
 }
 ];
